da4cdf0796fde5be7b8d27fb9d2226c73cf1281b
Pierre Chollet <67265207+PChol22@users.noreply.github.com>
2022-01-14 16:07:57 +0100

    progress

--- Query5/__init__.py
+++ Query5/__init__.py
@@ -27,7 +27,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             genre = req.params.get('genre') or req_body.get('genre')
             director = req.params.get('director') or req_body.get('director')
 
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+
+    errorMessage = ""
+    result1 = []
+
+    try:
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        filter1 = "n1.primaryName='{}'".format(actor) if actor else "true"
+        filter2 = "n2.primaryName='{}'".format(director) if director else "true"
+        producers = graph.run("MATCH (n1:Name)-[rel:ACTED_IN]->(t:Title)<-[rel2:DIRECTED]-(n2:Name) WITH DISTINCT t.tconst AS titleIds, n1, n2 WHERE {} AND {} RETURN titleIds".format(filter1, filter2))
+        for producer in producers:
+            result1.append(producer['titleIds'])
+    except:
+        errorMessage = "Erreur de connexion a la base Neo4j"
+
     dataString = "actor : {} - genre : {} - director : {}".format(actor or "Not given", genre or "Not given", director or "Not given")
+    dataString += str(data).strip('[]')
 
     return func.HttpResponse(dataString)
     
