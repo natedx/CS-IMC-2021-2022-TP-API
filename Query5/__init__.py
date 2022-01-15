eb299050de943fe0c49f485923813799e60e476a
Pierre Chollet <67265207+PChol22@users.noreply.github.com>
2022-01-15 11:38:10 +0100

    logging

--- Query5/__init__.py
+++ Query5/__init__.py
@@ -17,74 +17,74 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     genre = req.params.get('genre')
     director = req.params.get('director')
 
+
+    if not actor or not genre or not director:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            actor = req.params.get('actor') or req_body.get('actor')
+            genre = req.params.get('genre') or req_body.get('genre')
+            director = req.params.get('director') or req_body.get('director')
+
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+
+    errorMessage = ""
+    result1 = []
+
     try:
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        filter1 = "n1.primaryName='{}'".format(actor) if actor else "true"
+        filter2 = "n2.primaryName='{}'".format(director) if director else "true"
+        producers = graph.run("MATCH (n1:Name)-[rel:ACTED_IN]->(t:Title)<-[rel2:DIRECTED]-(n2:Name) WITH DISTINCT t.tconst AS titleIds, n1, n2 WHERE {} AND {} RETURN titleIds".format(filter1, filter2))
+        for producer in producers:
+            result1.append(str(producer['titleIds']))
+    except:
+        errorMessage = "Erreur de connexion a la base Neo4j"
 
-        if not actor or not genre or not director:
-            try:
-                req_body = req.get_json()
-            except ValueError:
-                pass
-            else:
-                actor = req.params.get('actor') or req_body.get('actor')
-                genre = req.params.get('genre') or req_body.get('genre')
-                director = req.params.get('director') or req_body.get('director')
+    if errorMessage != "":
+        return func.HttpResponse(errorMessage)
 
-        neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
-        neo4j_user = os.environ["TPBDD_NEO4J_USER"]
-        neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+    logging.info(str(result1).strip('[]'))
 
-        if len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
-            return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
-        errorMessage = ""
-        result1 = []
+    result2 = ""
 
-        try:
-            logging.info("Test de connexion avec py2neo...")
-            graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
-            filter1 = "n1.primaryName='{}'".format(actor) if actor else "true"
-            filter2 = "n2.primaryName='{}'".format(director) if director else "true"
-            producers = graph.run("MATCH (n1:Name)-[rel:ACTED_IN]->(t:Title)<-[rel2:DIRECTED]-(n2:Name) WITH DISTINCT t.tconst AS titleIds, n1, n2 WHERE {} AND {} RETURN titleIds".format(filter1, filter2))
-            for producer in producers:
-                result1.append(producer['titleIds'])
-        except:
-            errorMessage = "Erreur de connexion a la base Neo4j"
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
 
-        if errorMessage != "":
-            return func.HttpResponse(errorMessage)
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        filter3_1 = "JOIN tGenres ON tTitles.tconst = tGenres.tconst"
+        filter3_2 = "tGenres.genre = '{}'".format(genre or "1=1")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT SUM(tTitles.runtimeMinutes)/COUNT(*) FROM tTitles {} WHERE tTitles.tconst IN ({}) AND {}".format(filter3_1,str(result1).strip('[]'),filter3_2))
+            rows = cursor.fetchall()
+            for row in rows:
+                result2 += str(row[0])
+        logging.info("SQL request successful !")
+        logging.info(repr(rows))
 
-        server = os.environ["TPBDD_SERVER"]
-        database = os.environ["TPBDD_DB"]
-        username = os.environ["TPBDD_USERNAME"]
-        password = os.environ["TPBDD_PASSWORD"]
-        driver= '{ODBC Driver 17 for SQL Server}'
+    except:
+        errorMessage = "Erreur de connection à la base SQL"
 
-        result2 = ""
+    if errorMessage != "":
+        return func.HttpResponse(errorMessage)
 
-        if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
-            return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
-
-        try:
-            logging.info("Test de connexion avec pyodbc...")
-            filter3_1 = "JOIN tGenres ON tTitles.tconst = tGenres.tconst"
-            filter3_2 = "tGenres.genre = '{}'".format(genre or "1=1")
-            with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT SUM(tTitles.runtimeMinutes)/COUNT(*) FROM tTitles {} WHERE tTitles.tconst IN ({}) AND {}".format(filter3_1,str(result1).strip('[]'),filter3_2))
-                rows = cursor.fetchall()
-                for row in rows:
-                    result2 += str(row[0])
-
-        except:
-            errorMessage = "Erreur de connection à la base SQL"
-
-        if errorMessage != "":
-            return func.HttpResponse(errorMessage)
-
-        return func.HttpResponse(result2)
-
-    except Exception as e:
-        return func.HttpResponse(repr(e))
+    return func.HttpResponse(result2)
 
     
 
